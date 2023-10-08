d7979f6e111ee08fcad29ba53405705741e8227e
SafronovMaks1m <safranov01@list.ru>
2023-10-08 20:52:52 +0300

    init

--- 2-if_else_switch/LeapYear/leap_year.h
+++ 2-if_else_switch/LeapYear/leap_year.h
@@ -1,6 +1,9 @@
 int LeapYear(int year){
     if (year%4 == 0){
-        return 1;
-    }
-    return 0;
-}
+        if (year%100 == 0){
+            if (year%400 == 0)
+                return 1;
+            return 0;
+        }
+        return 1;}
+    return 0;}
