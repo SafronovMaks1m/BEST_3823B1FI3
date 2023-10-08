5a28623f90bc78410cc8e5e1d4e5ca966f3a9152
SafronovMaks1m <safranov01@list.ru>
2023-10-08 19:32:21 +0300

    init

--- 2-if_else_switch/Chess/chess.h
+++ 2-if_else_switch/Chess/chess.h
@@ -1,3 +1,17 @@
 int chess_rook(char a, int b, char c, int d){
+    char mas[8] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+    int i = 0, total = 0;
+    for (i = 0; i<8; i++){
+        if (a == mas[i]){
+            total+=1;}
+        if (c == mas[i]){
+            total+=1;}
+    }
+    if (b<=8 && d <=8 && total == 2){
+        if (a == c && b == d){
+            return 0;}
+        else if (a == c || b == d ){
+            return 1;}
+    }
     return 0;
 }
