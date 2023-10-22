49da720c748d515d3c4d465529a84bbc59fe79c3
SafronovMaks1m <safranov01@list.ru>
2023-10-22 21:26:26 +0300

    init

--- 3-loop/bin_patterns/find_bin_pattern.h
+++ 3-loop/bin_patterns/find_bin_pattern.h
@@ -1,5 +1,12 @@
 #include <math.h>
 
 char find_bin_pattern(int number){
-    return 100;
-}
+    int mas[100] = {}, total = 0, count = 0;
+    while (number>0){
+        mas[total] = number%2;
+        number = number/2;
+        total+=1;}
+    for (int i = 0; i<97; i++){
+        if (mas[i] == 1 && mas[i+1] == 0 && mas[i+2] == 1){
+            count+=1;}}
+    return count;}
