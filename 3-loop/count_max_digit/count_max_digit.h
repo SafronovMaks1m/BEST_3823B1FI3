34011131f013d8d3bedb034eb5bd2b2fbd53e75a
SafronovMaks1m <safranov01@list.ru>
2023-10-23 19:28:51 +0300

    init

--- 3-loop/count_max_digit/count_max_digit.h
+++ 3-loop/count_max_digit/count_max_digit.h
@@ -1,5 +1,19 @@
 #include <math.h>
 
-char count_max_digit(long long int){
-    return 100;
+char count_max_digit(long long number){
+    int max = -1000, mas[100] = {}, total = 0;
+    if (number<0){
+        number = abs(number);}
+    while (number>0){
+        mas[total] = number%10;
+        number/=10;
+        total+=1;}
+    for(int i = 0; i<100; i++){
+        if (mas[i]>max){
+            max = mas[i];}}
+    total = 0;
+    for(int i = 0; i<100; i++){
+        if (mas[i] == max){
+            total+=1;}}
+    return (char) total;
 }
