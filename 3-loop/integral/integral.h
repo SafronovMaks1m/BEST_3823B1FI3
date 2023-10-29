a04b169966f75f0e9768e47ee1a6d6d216cd7573
SafronovMaks1m <safranov01@list.ru>
2023-10-29 13:27:02 +0300

    init

--- 3-loop/integral/integral.h
+++ 3-loop/integral/integral.h
@@ -1,5 +1,12 @@
 #include <math.h>
 
 double integral(double a, double b, double c, double left, double right){
-    return 0.0;
+    double pl1, pl2, s;
+    pl1 = (a*pow(right, 3)/3 + b*pow(right, 2)/2 + c*right);
+    pl2 = (a*pow(left, 3)/3 + b*pow(left, 2)/2 + c*left);
+    s = pl1-pl2;
+    if (s>0)
+        return s;
+    else
+        return -1;
 }
