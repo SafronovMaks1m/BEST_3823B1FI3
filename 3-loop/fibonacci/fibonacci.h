3995af6addfedd881bc4cf6bca5a832aaf1053dc
SafronovMaks1m <safranov01@list.ru>
2023-10-29 13:17:20 +0300

    init

--- 3-loop/fibonacci/fibonacci.h
+++ 3-loop/fibonacci/fibonacci.h
@@ -1,3 +1,12 @@
 long fib(int m){
-    return 0;
+    int s = 0;
+    int f(m){
+        if (m == 0){
+            return 0;}
+        if (m == 1){
+            return 1;}
+        if (m > 2){
+            return F(m-2) + F(m-1);}}
+    s = f(m);
+    return s;
 }
--- 3-loop/find_digit/find_digit.h
+++ 3-loop/find_digit/find_digit.h
@@ -1,5 +1,41 @@
 #include <math.h>
-
+#include <stdlib.h>
+#include <stdio.h>
 char find_digit(double number, char digit){
-    return 100;
+    long long n; double m;
+    int total=0, mas[50] = {0}, arr[50] = {0}, t;
+    long long s, count = 0;
+    n = (long long) number;
+    m = number - n;
+    while (n>0){
+        mas[total] = n%10;
+        n = n/10;
+        total+=1;}
+    for (int i = 49; i>=0; i--){
+        if (mas[i]!=0){
+            t = i;
+            break;}}
+    for (int i = 49; i>=0; i--){
+        if (i<=t){
+            count+=1;
+            if (mas[i] == digit){
+                return count;}}}
+    count+=1;
+    m = m*1000000000000;
+    s = m;
+    total = 0;
+    while (s>0){
+        arr[total] = s%10;
+        s = s/10;
+        total+=1;}
+    for (int i = 49; i>=0; i--){
+        if (arr[i]!=0){
+            t = i;
+            break;}}
+    for (int i = 49; i>=0; i--){
+        if (i<=t){
+            count+=1;
+            if (arr[i] == digit){
+                return count;}}}
+    return -1;
 }
