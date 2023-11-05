9e552af66b41576325102808313deec42a9a832f
SafronovMaks1m <safranov01@list.ru>
2023-11-05 13:15:32 +0300

    init

--- 3-loop/fibonacci/fibonacci.h
+++ 3-loop/fibonacci/fibonacci.h
@@ -1,3 +1,12 @@
 long fib(int m){
-    return 1;
+    int prom = 0, f0 = 0, f1 = 1;
+    long long sum = 0;
+    if (m>=0){
+    while (prom<=m){
+        prom = f0+f1;
+        sum+=prom;
+        f1 = f0;
+        f0 = prom;}
+    return sum-prom;}
+    return -1;
 }
