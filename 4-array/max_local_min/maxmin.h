c9db52e96cb179f792b702d03a2d24530ac00a38
SafronovMaks1m <safranov01@list.ru>
2023-11-13 00:10:38 +0300

    init

--- 4-array/max_local_min/maxmin.h
+++ 4-array/max_local_min/maxmin.h
@@ -1,3 +1,19 @@
 int maxmin(int input[], int size){
-    return -1;
+    int max = -10000;
+    for (int i = 0; i<size; i++){
+        if (i == 0){
+            if (input[i+1]>input[i]){
+                if (input[i]>max){
+                max = input[i]; continue;}}}
+        if (i == size-1){
+            if (input[i-1]>input[i]){
+                if (input[i]>max){
+                max = input[i]; continue;}}}
+        if (input[i-1]>input[i] && input[i+1]>input[i]){
+            if (input[i]>max){
+                max = input[i];
+            }
+        }
+    }
+    return max;
 }
--- 4-array/more_on_left/left_more.h
+++ 4-array/more_on_left/left_more.h
@@ -1,3 +1,7 @@
 char left_more(double input[], unsigned int size){
-    return -1;
+    int total = 0;
+    for (int i = 1; i<size; i++){
+        if (input[i]>input[i-1]){
+            total+=1;}}
+    return total;
 }
