3313fafbacfde58f244b97a4ec8a5e46e1f6bd26
SafronovMaks1m <safranov01@list.ru>
2023-11-12 20:43:42 +0300

    init

--- 4-array/get_percent/calc_percent.h
+++ 4-array/get_percent/calc_percent.h
@@ -1,5 +1,15 @@
 #include <math.h>
 
 int calc_percent(int matrix[], int size){
-    return 0;
+    int max = -100000, min = 1000000, total = 0;
+    for (int i = 0; i<size; i++){
+        if (matrix[i]>max){
+            max = matrix[i];}
+        if (matrix[i]<min){
+            min = matrix[i];}}
+    double r = (max-min)/2;
+    for (int i = 0; i<size; i++){
+        if (matrix[i]>r){
+            total+=1;}}
+    return ((double)total/(double)size)*100;
 }
