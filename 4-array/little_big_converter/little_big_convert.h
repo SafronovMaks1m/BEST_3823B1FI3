a80a2a0251f135347a177d367d1e01bfc50f763e
SafronovMaks1m <safranov01@list.ru>
2023-11-13 19:59:01 +0300

    init

--- 4-array/little_big_converter/little_big_convert.h
+++ 4-array/little_big_converter/little_big_convert.h
@@ -1,5 +1,10 @@
 #include <math.h>
-//
-unsigned int little_big_convert(unsigned int input){ 
-    return 0;
+
+unsigned int little_big_convert(unsigned int input){
+    int littleEndian = ((input>>24)&0x000000ff) +
+                   ((input<<8)&0x00ff0000) +
+                   ((input>>8)&0x0000ff00) + 
+                   ((input<<24)&0xff000000);
+    return littleEndian;
+    
 }
