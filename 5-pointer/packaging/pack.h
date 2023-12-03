5a0888694582d45f02a64e5bbd7aa0ab90196f11
SafronovMaks1m <safranov01@list.ru>
2023-12-04 01:43:10 +0300

    init

--- 5-pointer/packaging/pack.h
+++ 5-pointer/packaging/pack.h
@@ -1,9 +1,13 @@
 #include <math.h>
 
 unsigned long long pack_ull(unsigned char arr[], int size){
-    return 100;
-}
+    if (size > 8) {
+        return 0;}
+    unsigned long long packed_value = 0;
+    for (int i = 0; i < size; i++) {
+        packed_value = packed_value + ((unsigned long long)arr[i] << (8 * i));}
+    return packed_value;}
 
 unsigned char unpack_ull(unsigned long long input){
-    return 5;
-}
+    unsigned char unpacked_byte = (input >> 32);
+    return unpacked_byte;}
