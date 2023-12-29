db5fb47d9006e5d46dee1f1d694f1be1b9ab7f7e
SafronovMaks1m <safranov01@list.ru>
2023-12-29 06:06:32 +0300

    init

--- 8-strings/to_lower/lower_converter.h
+++ 8-strings/to_lower/lower_converter.h
@@ -4,6 +4,5 @@
 void lower(char* str){
     for (int i = 0; str[i]!='\0'; i++){
         if ((int)str[i]>=65 && (int)str[i]<=90){
-            str[i]+=32;}}
-            
+            str[i]+=32;}}   
 }
