3258b34aada275efdd55e3b786c678dd30cd6d11
SafronovMaks1m <safranov01@list.ru>
2023-12-29 05:33:09 +0300

    init

--- 8-strings/str_to_double/str_to_double_convert.h
+++ 8-strings/str_to_double/str_to_double_convert.h
@@ -2,5 +2,13 @@
 #include <string.h>
 
 double convert(char num[]){
-    return 0.0;
+    long long total = 0, count = 0, i = 0, k = 0; int znak = 1; long double t = 0;
+    if (num[0] == '-'){
+        znak = -1; i++;}
+    while(num[i]!='.'){
+        total = total*10+((int)num[i]-48); i++;} i++;
+    for (i; num[i]!='\0'; i++){
+        count = count*10+((int)num[i]-48); k+=1;}
+    t = (total+count*pow(0.1, k))*znak;
+    return t;
 }
