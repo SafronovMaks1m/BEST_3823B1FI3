4fd0b2875a2b0a1265fcd8f1dd9841a1fd15c074
SafronovMaks1m <144445164+SafronovMaks1m@users.noreply.github.com>
2024-02-12 14:30:07 +0300

    Update change.h

--- 2-if_else_switch/Change/change.h
+++ 2-if_else_switch/Change/change.h
@@ -1,6 +1,41 @@
-long long change(int a, int b, int c, int d){
-    long  h;
-    h = ((long long)c*100+(long long)d) - ((long long)a*100+ (long long)b);
-    if (h>=0){
-        return h;}
-    return -1;}
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+
+
+void method_gaussa(int** matrix, int* vector_b, int size){
+    int* old_str = new int[size];
+    int ved_elem = 0;
+    for (int i = 0; i<size; i++){
+        for (int j = 0; i<size; i++){
+            for(int k = 0; j<size; j++){
+                if (matrix[j][k]!=0)(
+                    ved_elem = matrix[j][k];)
+            }
+        }
+    }
+}
+
+int main(){
+    int size;
+    mt19937 gen;
+    gen.seed(std::random_device()());
+    uniform_int_distribution<mt19937::result_type> dist;
+    cout << "Введите размер массива" << endl; cin >> size;
+    int* vector_b = new int[size]; 
+    int** matrix = new int* [size];
+    for (int i = 0; i<size; i++){
+        for (int j = 0; j<5; j++){
+            matrix[i] = new int [5];
+        }
+    }
+    for (int i = 0; i<size; i++){
+        for (int j = 0; j<5; j++){
+            matrix[i][j] = dist(gen)%10;
+        }
+    }
+
+
+}
